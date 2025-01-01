f6ae22b7ec67e9411449065c752c74486aba63b7
esthercate <vuthi.esther@gmail.com>
2025-01-01 19:23:52 +0300

    Add notifcation

--- app/components/common/ContactForm.tsx
+++ app/components/common/ContactForm.tsx
@@ -3,6 +3,8 @@
 import { ContactCard } from "../common/styles";
 import React, { useRef, useEffect } from "react";
 import emailjs from "@emailjs/browser";
+import { ToastContainer, toast, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 type Props = {};
 
@@ -18,9 +20,23 @@ const ContactForm = (props: Props) => {
       })
       .then(
         () => {
-          console.log("SUCCESS!");
+          toast.success("Message Sent successfully!", {
+            position: "bottom-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            transition: Slide,
+          });
+          if (form.current) {
+            form.current.reset();
+          }
         },
         (error) => {
+          toast.error("Failed to send message");
           console.log("FAILED...", error.text);
         }
       );
@@ -41,18 +57,21 @@ const ContactForm = (props: Props) => {
           placeholder="Name"
           name="from_name"
           className="border-b border-gray-300 mb-4 bg-bg-gray focus:outline-none"
+          required
         />
         <input
           type="email"
           placeholder="Email"
           name="from_email"
           className="border-b border-gray-300 mb-4 bg-bg-gray focus:outline-none"
+          required
         />
         <textarea
           placeholder="Message"
           name="message"
           className="border-b border-gray-300 mb-4 bg-bg-gray focus:outline-none"
           rows={2}
+          required
         />
         <input
           type="submit"
@@ -60,6 +79,19 @@ const ContactForm = (props: Props) => {
           className="py-2 px-4 border-2 border-orange m-auto rounded-lg flex justify-center items-center mt-3 text-orange hover:text-white cursor-pointer"
         />
       </form>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Slide}
+      />
     </ContactCard>
   );
 };
