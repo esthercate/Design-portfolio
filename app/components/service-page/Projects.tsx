a7605c9b8bccf6a8824456cd72bc1a2286ce4d6d
esthercate <vuthi.esther@gmail.com>
2025-04-02 08:59:12 +0300

    Add projects component

---
+++ app/components/service-page/Projects.tsx
@@ -0,0 +1,68 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import hospitalFinder from '../../../public/hospitalfinder.png';
+
+type Props = {};
+
+const Projects = (props: Props) => {
+	return (
+		<div className="flex flex-col px-5 tablet:px-16 py-10 w-full gap-6">
+			<div className="flex gap-8 justify-center">
+				<div className="text-orange underline cursor-pointer">All</div>
+				<div className="cursor-pointer hover:text-orange hover:underline">
+					Development
+				</div>
+				<div className="cursor-pointer hover:text-orange hover:underline">
+					Maintenance
+				</div>
+				<div className="cursor-pointer hover:text-orange hover:underline">
+					UI/UX
+				</div>
+				<div className="cursor-pointer hover:text-orange hover:underline">
+					SEO
+				</div>
+			</div>
+			<div className="w-full flex flex-col gap-4">
+				<div className="flex">
+					<div className="w-2/5">
+						<Image
+							src={hospitalFinder}
+							width={1005}
+							height={500}
+							alt="Hospital finder image"
+							className="rounded-l-lg"
+						/>
+					</div>
+					<div className="bg-bg-gray w-2/5">Desription</div>
+				</div>
+				<div className="flex w-full">
+					<div className="bg-bg-gray w-2/5">Desription</div>
+					<div className="w-2/5">
+						<Image
+							src={hospitalFinder}
+							width={1005}
+							height={500}
+							alt="Hospital finder image"
+							className="rounded-l-lg"
+						/>
+					</div>
+				</div>
+				<div className="flex">
+					<div className="w-2/5">
+						<Image
+							src={hospitalFinder}
+							width={1005}
+							height={500}
+							alt="Hospital finder image"
+							className="rounded-l-lg"
+						/>
+					</div>
+					<div className="bg-bg-gray w-2/5">Desription</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Projects;
