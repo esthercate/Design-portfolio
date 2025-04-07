c88b2d4d5f5a8287d314e5cbe3c686b57325e75d
esthercate <vuthi.esther@gmail.com>
2025-04-07 22:27:49 +0300

    Feat: blog changes, projects cards

--- app/components/common/BlogCard.tsx
+++ app/components/common/BlogCard.tsx
@@ -20,41 +20,45 @@ type Props = {
 
 const BlogCard = ({ blogPosts }: Props) => {
   return (
-    <>
-      {blogPosts.length > 0 &&
-        blogPosts.map(
-          ({ image, title, description, author, datePosted, url, id }) => (
-            <PortfolioCard
-              className="w-full tablet:w-[360px] flex flex-col"
-              key={id}
-            >
-              <Image
-                src={image}
-                width={400}
-                height={400}
-                alt="Blog image"
-                className="rounded-t-lg h-40 tablet:h-36 w-full"
-              />
-              <div className="bg-bg-gray">
-                <div className="px-2 py-3 flex flex-col gap-3">
-                  <Link href={url} className="font-bold text-sm hover:text-orange" target="_blank">
-                    {title}
-                  </Link>
-                  <p className="text-xs">{description}</p>
-                </div>
-                <div className="px-2 py-3 flex gap-2 items-center">
-                  <FaUserCircle size={40} />
-                  <div>
-                    <p className="font-bold text-xs">{author}</p>
-                    <p className="text-xs">{datePosted}</p>
-                  </div>
-                </div>
-              </div>
-            </PortfolioCard>
-          )
-        )}
-    </>
-  );
+		<>
+			{blogPosts.length > 0 &&
+				blogPosts.map(
+					({ image, title, description, author, datePosted, url, id }) => (
+						<PortfolioCard
+							className="w-full tablet:w-[360px] flex flex-col"
+							key={id}
+						>
+							<Image
+								src={image}
+								width={400}
+								height={400}
+								alt="Blog image"
+								className="rounded-t-lg h-40 tablet:h-36 w-full"
+							/>
+							<div className="bg-bg-gray h-full">
+								<div className="px-2 py-3 flex flex-col gap-3">
+									<Link
+										href={url}
+										className="font-bold text-sm hover:text-orange"
+										target="_blank"
+									>
+										{title}
+									</Link>
+									<p className="text-xs">{description}</p>
+								</div>
+								<div className="px-2 py-3 flex gap-2 items-center">
+									<FaUserCircle size={40} />
+									<div>
+										<p className="font-bold text-xs">{author}</p>
+										<p className="text-xs">{datePosted}</p>
+									</div>
+								</div>
+							</div>
+						</PortfolioCard>
+					)
+				)}
+		</>
+	);
 };
 
 export default BlogCard;
--- app/components/service-page/Projects.tsx
+++ app/components/service-page/Projects.tsx
@@ -2,6 +2,46 @@
 import React from 'react';
 import Image from 'next/image';
 import hospitalFinder from '../../../public/hospitalfinder.png';
+import { MdOutlineArrowOutward } from 'react-icons/md';
+
+type Project = {
+	id: number;
+	title: string;
+	description: string;
+	image: any;
+	imagePosition: 'left' | 'right';
+	skills: string[];
+};
+
+const projectsData: Project[] = [
+	{
+		id: 1,
+		title: 'Hospital Finder',
+		description:
+			'Website to allow users to find nearest hospital, to find nearest hospital. Website to allow users to find nearest hospital.',
+		image: hospitalFinder,
+		imagePosition: 'left',
+		skills: ['UI/UX', 'Development', 'Maintenance', 'SEO'],
+	},
+	{
+		id: 2,
+		title: 'Hospital Finder',
+		description:
+			'Website to allow users to find nearest hospital, to find nearest hospital. Website to allow users to find nearest hospital.',
+		image: hospitalFinder,
+		imagePosition: 'right',
+		skills: ['UI/UX', 'Development', 'Maintenance', 'SEO'],
+	},
+	{
+		id: 3,
+		title: 'Hospital Finder',
+		description:
+			'Website to allow users to find nearest hospital, to find nearest hospital. Website to allow users to find nearest hospital.',
+		image: hospitalFinder,
+		imagePosition: 'left',
+		skills: ['UI/UX', 'Development', 'Maintenance', 'SEO'],
+	},
+];
 
 type Props = {};
 
@@ -11,58 +51,69 @@ const Projects = (props: Props) => {
 			className="flex flex-col px-5 tablet:px-16 py-10 w-full gap-6"
 			id="portfolio"
 		>
-			<div className="flex gap-8 justify-center text-sm">
-				<div className="text-orange underline cursor-pointer">All</div>
-				<div className="cursor-pointer hover:text-orange hover:underline">
-					Development
-				</div>
-				<div className="cursor-pointer hover:text-orange hover:underline">
-					Maintenance
-				</div>
-				<div className="cursor-pointer hover:text-orange hover:underline">
-					UI/UX
-				</div>
-				<div className="cursor-pointer hover:text-orange hover:underline">
-					SEO
-				</div>
-			</div>
 			<div className="w-full flex flex-col gap-4">
-				<div className="flex">
-					<div className="w-2/5">
-						<Image
-							src={hospitalFinder}
-							width={1005}
-							height={500}
-							alt="Hospital finder image"
-							className="rounded-l-lg"
-						/>
+				{projectsData.map((project) => (
+					<div
+						key={project.id}
+						className="flex w-full"
+					>
+						{project.imagePosition === 'left' ? (
+							<>
+								<div className="w-2/5">
+									<Image
+										src={project.image}
+										width={1005}
+										height={500}
+										alt={`${project.title} image`}
+										className="rounded-l-lg"
+									/>
+								</div>
+								<div className="bg-bg-gray w-2/5 px-4 py-2 flex flex-col gap-4">
+									<div className="bg-bg-dark rounded-lg p-1 max-w-fit">
+										<MdOutlineArrowOutward
+											color="orange"
+											size={30}
+										/>
+									</div>
+									<h5>{project.title}</h5>
+									<p>{project.description}</p>
+									<ul className="list-disc list-inside flex gap-4 text-xs text-orange">
+										{project.skills.map((skill, index) => (
+											<li key={index}>{skill}</li>
+										))}
+									</ul>
+								</div>
+							</>
+						) : (
+							<>
+								<div className="bg-bg-gray w-2/5 px-4 py-2 flex flex-col gap-4">
+									<div className="bg-bg-dark rounded-lg p-1 max-w-fit">
+										<MdOutlineArrowOutward
+											color="orange"
+											size={30}
+										/>
+									</div>
+									<h5>{project.title}</h5>
+									<p>{project.description}</p>
+									<ul className="list-disc list-inside flex gap-4 text-xs text-orange">
+										{project.skills.map((skill, index) => (
+											<li key={index}>{skill}</li>
+										))}
+									</ul>
+								</div>
+								<div className="w-2/5">
+									<Image
+										src={project.image}
+										width={1005}
+										height={500}
+										alt={`${project.title} image`}
+										className="rounded-r-lg"
+									/>
+								</div>
+							</>
+						)}
 					</div>
-					<div className="bg-bg-gray w-2/5">Desription</div>
-				</div>
-				<div className="flex w-full">
-					<div className="bg-bg-gray w-2/5">Desription</div>
-					<div className="w-2/5">
-						<Image
-							src={hospitalFinder}
-							width={1005}
-							height={500}
-							alt="Hospital finder image"
-							className="rounded-l-lg"
-						/>
-					</div>
-				</div>
-				<div className="flex">
-					<div className="w-2/5">
-						<Image
-							src={hospitalFinder}
-							width={1005}
-							height={500}
-							alt="Hospital finder image"
-							className="rounded-l-lg"
-						/>
-					</div>
-					<div className="bg-bg-gray w-2/5">Desription</div>
-				</div>
+				))}
 			</div>
 		</div>
 	);
