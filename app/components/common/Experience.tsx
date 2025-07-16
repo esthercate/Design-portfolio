350335e320c205f95afaa8fd083c926b1fcdac43
esthercate <vuthi.esther@gmail.com>
2025-07-16 10:19:07 +0300

    Add current job to portfolio

--- app/components/common/Experience.tsx
+++ app/components/common/Experience.tsx
@@ -12,42 +12,57 @@ type Props = {};
 
 const Experience = (props: Props) => {
   return (
-    <div className="flex flex-col gap-4">
-      <div>
-        <h3>My Experience</h3>
-      </div>
-      <VerticalTimeline layout="1-column-left">
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "#1b1b1c", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid #1b1b1c" }}
-          date="Jan 2024 - Present"
-          iconStyle={{ background: "#fe930e", color: "#fff" }}
-          icon={<FaCodeBranch />}
-          visible={true}
-        >
-          <h4 className="vertical-timeline-element-title text-white">
-            Mid-Level Software Engineer
-          </h4>
-          <h5 className="vertical-timeline-element-subtitle">- Remote</h5>
-          <p>StartupBlink - Haifa, Israel</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "#1b1b1c", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid #1b1b1c" }}
-          date="Oct 2022 - Dec 2023"
-          iconStyle={{ background: "#fe930e", color: "#fff" }}
-          icon={<FaCode />}
-          visible={true}
-        >
-          <h4 className="vertical-timeline-element-title">
-            Junior Software Engineer
-          </h4>
-          <h5 className="vertical-timeline-element-subtitle">- Remote</h5>
-          <p>StartupBlink - Haifa, Israel</p>
-        </VerticalTimelineElement>
-        {/* <VerticalTimelineElement
+		<div className="flex flex-col gap-4">
+			<div>
+				<h3>My Experience</h3>
+			</div>
+			<VerticalTimeline layout="1-column-left">
+				<VerticalTimelineElement
+					className="vertical-timeline-element--work"
+					contentStyle={{ background: '#1b1b1c', color: '#fff' }}
+					contentArrowStyle={{ borderRight: '7px solid #1b1b1c' }}
+					date="May 2025 - Present"
+					iconStyle={{ background: '#fe930e', color: '#fff' }}
+					icon={<FaCodeBranch />}
+					visible={true}
+				>
+					<h4 className="vertical-timeline-element-title text-white">
+						Full Stack Software Engineer
+					</h4>
+					<h5 className="vertical-timeline-element-subtitle">- Remote</h5>
+					<p>Freelancer - Kenya, Nairobi</p>
+				</VerticalTimelineElement>
+				<VerticalTimelineElement
+					className="vertical-timeline-element--work"
+					contentStyle={{ background: '#1b1b1c', color: '#fff' }}
+					contentArrowStyle={{ borderRight: '7px solid #1b1b1c' }}
+					date="Jan 2024 - Apr 2025"
+					iconStyle={{ background: '#fe930e', color: '#fff' }}
+					icon={<FaCodeBranch />}
+					visible={true}
+				>
+					<h4 className="vertical-timeline-element-title text-white">
+						Mid-Level Software Engineer
+					</h4>
+					<h5 className="vertical-timeline-element-subtitle">- Remote</h5>
+					<p>StartupBlink - Haifa, Israel</p>
+				</VerticalTimelineElement>
+				<VerticalTimelineElement
+					className="vertical-timeline-element--work"
+					contentStyle={{ background: '#1b1b1c', color: '#fff' }}
+					contentArrowStyle={{ borderRight: '7px solid #1b1b1c' }}
+					date="Oct 2022 - Dec 2023"
+					iconStyle={{ background: '#fe930e', color: '#fff' }}
+					icon={<FaCode />}
+					visible={true}
+				>
+					<h4 className="vertical-timeline-element-title">
+						Junior Software Engineer
+					</h4>
+					<h5 className="vertical-timeline-element-subtitle">- Remote</h5>
+					<p>StartupBlink - Haifa, Israel</p>
+				</VerticalTimelineElement>
+				{/* <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{ background: "#1b1b1c", color: "#fff" }}
           contentArrowStyle={{ borderRight: "7px solid #1b1b1c" }}
@@ -60,9 +75,9 @@ const Experience = (props: Props) => {
           <h5 className="vertical-timeline-element-subtitle">- On-site</h5>
           <p>Milimani Law Courts - Nairobi, Kenya</p>
         </VerticalTimelineElement> */}
-      </VerticalTimeline>
-    </div>
-  );
+			</VerticalTimeline>
+		</div>
+	);
 };
 
 export default Experience;
