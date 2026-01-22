3bd76cd4f1dbe21a3b06566cead98916da17e434
esthercate <vuthi.esther@gmail.com>
2026-01-22 14:32:45 +0300

    Update to current experience

--- app/components/common/Experience.tsx
+++ app/components/common/Experience.tsx
@@ -4,9 +4,7 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import { MdOutlineSettings, MdOutlineWork } from "react-icons/md";
-import { FaCode } from "react-icons/fa";
-import { FaCodeBranch } from "react-icons/fa";
+import { FaCode, FaReact, FaCodeBranch } from "react-icons/fa";
 
 type Props = {};
 
@@ -21,16 +19,16 @@ const Experience = (props: Props) => {
 					className="vertical-timeline-element--work"
 					contentStyle={{ background: '#1b1b1c', color: '#fff' }}
 					contentArrowStyle={{ borderRight: '7px solid #1b1b1c' }}
-					date="May 2025 - Present"
+					date="Jun 2025 - Present"
 					iconStyle={{ background: '#fe930e', color: '#fff' }}
-					icon={<FaCodeBranch />}
+					icon={<FaReact />}
 					visible={true}
 				>
 					<h4 className="vertical-timeline-element-title text-white">
-						Full Stack Software Engineer
+						Frontend Developer | React and Next.js
 					</h4>
-					<h5 className="vertical-timeline-element-subtitle">- Remote</h5>
-					<p>Freelancer - Kenya, Nairobi</p>
+					<h5 className="vertical-timeline-element-subtitle">- Kenya, Nairobi</h5>
+					<p>Independent Contractor</p>
 				</VerticalTimelineElement>
 				<VerticalTimelineElement
 					className="vertical-timeline-element--work"
@@ -62,19 +60,6 @@ const Experience = (props: Props) => {
 					<h5 className="vertical-timeline-element-subtitle">- Remote</h5>
 					<p>StartupBlink - Haifa, Israel</p>
 				</VerticalTimelineElement>
-				{/* <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "#1b1b1c", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid #1b1b1c" }}
-          date="Oct 2020 - Sep 2021"
-          iconStyle={{ background: "#fe930e", color: "#fff" }}
-          icon={<MdOutlineSettings />}
-          visible={true}
-        >
-          <h4 className="vertical-timeline-element-title">ICT Officer</h4>
-          <h5 className="vertical-timeline-element-subtitle">- On-site</h5>
-          <p>Milimani Law Courts - Nairobi, Kenya</p>
-        </VerticalTimelineElement> */}
 			</VerticalTimeline>
 		</div>
 	);
